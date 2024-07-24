393208205209ac8922de29f776fdf9b0e549ad8d
ican0422 <ican0422@gmail.com>
2024-07-24 20:31:57 +0900

    2-2 클래스 환경 구축 모든 기능 작동

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -9,6 +9,8 @@ public class App {
         ArrayList<Integer> results = new ArrayList<Integer>();
         Scanner sc = new Scanner(System.in);
 
+        Calculator ca = new Calculator();
+
         /* 반복 계산 구현 */
         boolean isRunning = true;
 
@@ -31,48 +33,53 @@ public class App {
             /* 버퍼 초기화 */
             sc.nextLine();
 
-            /* 계산결과 변수 초기화 */
-            int sum = 0;
-            /* 사칙 연산 제어문 */
-            if(operator == '+') {
-                sum = num1 + num2;
-            }
-            if(operator == '-') {
-                sum = num1 - num2;
-            }
-            if(operator == '*') {
-                sum = num1 * num2;
-            }
-            if(operator == '/') {
-                sum = num1 / num2;
-            }
-            if(operator != '+' && operator != '-' && operator != '*' && operator != '/') {
-                System.out.println("올바르지 않은 기호를 입력하셨습니다.");
-                sum = 0;
-            }
+//            /* 계산결과 변수 초기화 */
+//            int sum = 0;
+//            /* 사칙 연산 제어문 */
+//            if(operator == '+') {
+//                sum = num1 + num2;
+//            }
+//            if(operator == '-') {
+//                sum = num1 - num2;
+//            }
+//            if(operator == '*') {
+//                sum = num1 * num2;
+//            }
+//            if(operator == '/') {
+//                sum = num1 / num2;
+//            }
+//            if(operator != '+' && operator != '-' && operator != '*' && operator != '/') {
+//                System.out.println("올바르지 않은 기호를 입력하셨습니다.");
+//                sum = 0;
+//            }
+            int sum = ca.calculate(num1,num2,operator);
+
             /* 결과 도출 */
             System.out.println("결과 : " + sum);
 
             /* 결과 List 배열 저장 */
-            results.add(sum);
+//            results.add(sum);
+            ca.getResults(sum);
 
             /* 연산 결과 삭제 */
             System.out.println("가장 먼저 저장된 연산 결과를 삭제 하시겠습니까? (remove 입력)");
             String remove = sc.nextLine();
-            if (remove.equals("remove")){
-                results.remove(0);
-            } else {
-                System.out.println("잘 못된 입력입니다.");
-            }
+//            if (remove.equals("remove")){
+//                results.remove(0);
+//            } else {
+//                System.out.println("잘 못된 입력입니다.");
+//            }
+            ca.remove(remove);
 
             /* 저장된 연산 결과 전부 조회 */
             System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
             String inquiry = sc.nextLine();
-            if (inquiry.equals("inquiry")){
-                System.out.println("저장된 연산 결과 : " + results.toString());
-            } else {
-                System.out.println("잘 못된 입력입니다.");
-            }
+//            if (inquiry.equals("inquiry")){
+//                System.out.println("저장된 연산 결과 : " + results.toString());
+//            } else {
+//                System.out.println("잘 못된 입력입니다.");
+//            }
+            ca.inquiry(inquiry);
 
             /* 종료 여부 확인 */
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,55 @@
 package calculator;
 
-public class Calculator {
+import java.util.ArrayList;
 
+public class Calculator {
+    ArrayList<Integer> results = new ArrayList<Integer>();
+    public int calculate(int num1, int num2, char operator){
+
+        /* 계산결과 변수 초기화 */
+        int sum = 0;
+        /* 사칙 연산 제어문 */
+        if(operator == '+') {
+            sum = num1 + num2;
+        }
+        if(operator == '-') {
+            sum = num1 - num2;
+        }
+        if(operator == '*') {
+            sum = num1 * num2;
+        }
+        if(operator == '/') {
+            sum = num1 / num2;
+        }
+        if(operator != '+' && operator != '-' && operator != '*' && operator != '/') {
+            System.out.println("올바르지 않은 기호를 입력하셨습니다.");
+            sum = 0;
+        }
+
+        return sum;
+    }
+
+    public ArrayList<Integer> getResults(int sum) {
+
+        /* 결과 List 배열 저장 */
+        results.add(sum);
+
+        return results;
+    }
+
+    public void remove(String remove){
+        if (remove.equals("remove")){
+            results.remove(0);
+        } else {
+            System.out.println("잘 못된 입력입니다.");
+        }
+    }
+
+    public void inquiry(String inquiry){
+        if (inquiry.equals("inquiry")){
+            System.out.println("저장된 연산 결과 : " + results.toString());
+        } else {
+            System.out.println("잘 못된 입력입니다.");
+        }
+    }
 }
