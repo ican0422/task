d9a29d4038e340e2445904c410db474e0b6c041f
ican0422 <ican0422@gmail.com>
2024-07-24 17:31:12 +0900

    1-6 추가, 출력

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -7,6 +7,7 @@ public class App {
     public static void main(String[] args) {
         int[] results = new int[10];
         int count = 0;
+        int n = 0;
         Scanner sc = new Scanner(System.in);
 
         /* 반복 계산 구현 */
@@ -48,20 +49,29 @@ public class App {
             }
             /* 결과 도출 */
             System.out.println("결과 : " + sum);
-            results[count] = sum;
-            System.out.println("배열 : " + results[0] + ", 배열 인덱스 : " + results[count]);
-            System.out.println("배열의 넣을때 인덱스 : " + count);
-            count++;
-            System.out.println("넣고 난 후 인덱스 : " + count);
 
+            /* 결과 배열 저장 */
+            results[count] = sum;
+            /* 배열이 꽉찼을땐 count는 10 여기서 계속 계산을 진행한다면 if-else문 발동 */
+            count++;
+
+            /* 종료 여부 확인 */
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             String exit = sc.nextLine();
 
             if (exit.equals("exit")) {
                 System.out.println("계산을 종료 하셨습니다.");
                 return;
+            } else {
+                /* 계산을 11번째 시도할때 배열을 이동시켜 배열의 빈공간 창출 */
+                if(count == 10){
+                    for (int i = 1; i < results.length; i++){
+                        results[n] = results[i];
+                        n++;
+                    }
+                    count = 9;
+                }
             }
-
         }
     }
 }
