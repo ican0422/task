b16bef9628499ff89be89b29f296c58e8ffa7d04
ican0422 <ican0422@gmail.com>
2024-07-24 23:50:56 +0900

    2-2 주석 및 코드 리펙토링

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 
 public class Calculator {
     ArrayList<Integer> results = new ArrayList<Integer>();
+    /* 계산결과 변수 초기화 */
+    int sum = 0;
+
+    /* 연산 함수 */
     public int calculate(int num1, int num2, char operator){
 
-        /* 계산결과 변수 초기화 */
-        int sum = 0;
+
         /* 사칙 연산 제어문 */
         if(operator == '+') {
             sum = num1 + num2;
@@ -29,14 +32,15 @@ public class Calculator {
         return sum;
     }
 
-    public ArrayList<Integer> getResults(int sum) {
-
+    /* 연산 결과 저장 함수 */
+    private ArrayList<Integer> results(int sum) {
         /* 결과 List 배열 저장 */
         results.add(sum);
 
         return results;
     }
 
+    /* 연산 결과 가장 먼저 입력된 수를 삭제하는 함수 */
     public void remove(String remove){
         if (remove.equals("remove")){
             results.remove(0);
@@ -45,6 +49,7 @@ public class Calculator {
         }
     }
 
+    /* 저장된 연산 결과 출력하는 함수 */
     public void inquiry(String inquiry){
         if (inquiry.equals("inquiry")){
             System.out.println("저장된 연산 결과 : " + results.toString());
@@ -52,4 +57,13 @@ public class Calculator {
             System.out.println("잘 못된 입력입니다.");
         }
     }
+
+    /* 연산 결과 저장 함수 캡슐화 */
+    public int getResults(){
+        return sum;
+    }
+    public void setResults(int sum){
+        this.sum = sum;
+    }
+
 }
